c389866f6ab29572f6cf17d3ff758274ed27164f
yandil <topiary@openmailbox.org>
2015-05-23 15:31:55 -0700

    starting gui code

---
+++ gui.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import Tkinter as tk
+
+
+class Application(tk.Frame):
+
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.grid()
+        self.createWidgets()
+
+    def createWidgits(self):
+        self.quitButton = tk.Button(self, text='Quit', command=self.quit)
+        self.quitButton.grid()
+
+
+if __name__ == '__main__':
+
+    app = Application()
+    app.master.title('KinderCrypt')
+    app.mainloop()
