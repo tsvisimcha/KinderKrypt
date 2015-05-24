4571c78e6887bae3b37c411cd81db5082c726801
yandil <topiary@openmailbox.org>
2015-05-23 19:24:14 -0700

    more options for key management

--- gui.py
+++ gui.py
@@ -10,31 +10,61 @@ class KeyManagementMenu(tk.Frame):
     """
 
     def __init__(self, parent=None, *args, **kwargs):
-        tk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent, )
         self.parent = parent
         self.grid()
         self.create_widgets()
 
     def create_widgets(self):
-        
+        """ Create portions of the key management menu.
+        """
         menubar = tk.Menu(self.parent)
         self.parent.config(menu=menubar)
-
-        key_mgmt_menu = tk.Menu(menubar)
-
+        key_mgmt_menu = tk.Menu(menubar)  # instantiate menu item
+        # add a list of commands to drop down menu
         key_mgmt_menu.add_command(
-            label='Generate Secret Key', command=lambda: "666")
+            label='Generate Key', command=self.generate_key_window)
+        key_mgmt_menu.add_command(
+            label='Export Key', command=self.export_key_window)
+        key_mgmt_menu.add_command(
+            label='Scan Key', command=self.scan_key_window)
+
         menubar.add_cascade(label="Key Management", menu=key_mgmt_menu)
-        # self.key_mangagement_menu = KeyManagementMenu()
 
-    def generate_key():
+    def generate_key_window(self):
         """
         """
+        self.top = tk.Toplevel(self.parent)
+        self.label = tk.Label(self.top, text="Generate Key")
+        self.label.pack()
+        self.name_entry = tk.Entry(self.top)
+        self.name_entry.pack()
 
-        return NotImplementedError
+        self.key_size_list = tk.Listbox(self.parent)
+        self.key_size_list.insert(tk.END, "entry")
+        self.key_size_list.pack()
+
+        self.button = tk.Button(
+            self.top, text="Generate", command=self.cleanup)
+        self.button.pack()
+        # done
+
+    def export_key_window(self):
+        pass
+
+    def scan_key_window(self):
+        pass
+
+    def cleanup(self):
+        self.value = self.name_entry.get()
+        self.top.destroy()
+        # done
 
 
-class EncryptionDecrytionMenu(tk.Frame):
+class EncryptionDecryptionMenu(tk.Frame):
+
+    """
+    """
 
     def __init__(self, parent=None):
 
@@ -45,6 +75,15 @@ class SigningAndVerificationMenu(tk.Frame):
 
     def __init__(self, parent=None):
 
+        pass
+
+
+class HelpMenu(tk.Frame):
+
+    def __init__(self, parent=None):
+        pass
+
+    def generate_help_window(self):
         pass
 
 
@@ -59,6 +98,8 @@ class StatusBar(tk.Frame):
 
 
 class PrimaryApplication(tk.Frame):
+    """ Primary application code.
+    """
 
     def __init__(self, parent=None, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
@@ -71,10 +112,10 @@ class PrimaryApplication(tk.Frame):
         # QUIT button
         self.quitButton = tk.Button(self, text='Quit', command=self.quit)
         self.quitButton['fg'] = 'red'
-        # self.quitButton.pack({'side': 'left'})
         self.quitButton.grid()
 
         self.key_menu = KeyManagementMenu(parent=self.parent)
+        self.help_menu = HelpMenu(parent=self.parent)
 
 
 if __name__ == '__main__':
