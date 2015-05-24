56a016c07de506ffc293fa27626ce883211eaf02
yandil <topiary@openmailbox.org>
2015-05-23 18:20:01 -0700

    key management menu

--- gui.py
+++ gui.py
@@ -3,33 +3,45 @@
 import Tkinter as tk
 
 
-class KeyManagementBar(tk.Frame):
+class KeyManagementMenu(tk.Frame):
 
-    def __init__(self, parent=None):
+    """ This part of the application for generating (creating) keys,
+        listing keys, deleting keys, and importing and exporting keys.
+    """
+
+    def __init__(self, parent=None, *args, **kwargs):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
         self.grid()
-        self.create_widgets
-        pass
+        self.create_widgets()
 
     def create_widgets(self):
-        self.generate_key_button = tk.Button(self,
-                                             text="Generate Secret Key",
-                                             command=self.generate_key)
+        
+        menubar = tk.Menu(self.parent)
+        self.parent.config(menu=menubar)
+
+        key_mgmt_menu = tk.Menu(menubar)
+
+        key_mgmt_menu.add_command(
+            label='Generate Secret Key', command=lambda: "666")
+        menubar.add_cascade(label="Key Management", menu=key_mgmt_menu)
+        # self.key_mangagement_menu = KeyManagementMenu()
 
     def generate_key():
-        """ Generate key pairs.
+        """
         """
 
         return NotImplementedError
 
 
-class EncryptionDecrytionBar(tk.Frame):
+class EncryptionDecrytionMenu(tk.Frame):
 
     def __init__(self, parent=None):
 
         pass
 
 
-class SigningAndVerificationBar(tk.Frame):
+class SigningAndVerificationMenu(tk.Frame):
 
     def __init__(self, parent=None):
 
@@ -50,22 +62,24 @@ class PrimaryApplication(tk.Frame):
 
     def __init__(self, parent=None, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
         self.grid()
         self.create_widgets()
-        # self.statusbar = StatusBar()
-        # self.navigation = NavBar()
 
     def create_widgets(self):
+
+        # QUIT button
         self.quitButton = tk.Button(self, text='Quit', command=self.quit)
         self.quitButton['fg'] = 'red'
         # self.quitButton.pack({'side': 'left'})
         self.quitButton.grid()
 
-    # TODO: create buttons for generating a PGP key
+        self.key_menu = KeyManagementMenu(parent=self.parent)
 
 
 if __name__ == '__main__':
-
-    app = PrimaryApplication()
+    root = tk.Tk()
+    root.geometry("550x550+300+300")
+    app = PrimaryApplication(parent=root)
     app.master.title('KinderCrypt')
     app.mainloop()
